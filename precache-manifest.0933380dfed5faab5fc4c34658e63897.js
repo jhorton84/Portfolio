e697c5effc070d5a776160624cbd073857e88244
Justin Horton <justinhorton@justins-mbp.lan>
2019-02-24 09:57:16 -0700

    Updates

---
+++ precache-manifest.0933380dfed5faab5fc4c34658e63897.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "be8fde40b59483d69b80d55824458d4c",
+    "url": "/Portfolio/static/media/icons8-npm.be8fde40.svg"
+  },
+  {
+    "revision": "88934bb35ebe8dda6e2a",
+    "url": "/Portfolio/static/css/main.8e5ead9b.chunk.css"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "9f4fc61665c0713069eb",
+    "url": "/Portfolio/static/js/2.9f4fc616.chunk.js"
+  },
+  {
+    "revision": "cc6dec76c1ac0fe892ee11fdc5125cea",
+    "url": "/Portfolio/static/media/icons8-html-5.cc6dec76.svg"
+  },
+  {
+    "revision": "a861f00352e24640d84add0f3d9393cb",
+    "url": "/Portfolio/static/media/icons8-github.a861f003.svg"
+  },
+  {
+    "revision": "5f11f090859121a4d90d4613afd1a010",
+    "url": "/Portfolio/static/media/icons8-css3.5f11f090.svg"
+  },
+  {
+    "revision": "1a2e6932e63ee4637912ab6d92eccce5",
+    "url": "/Portfolio/static/media/reactIcon.1a2e6932.svg"
+  },
+  {
+    "revision": "9366806d327dc277c8adfdee5896fd30",
+    "url": "/Portfolio/static/media/icons8-javascript.9366806d.svg"
+  },
+  {
+    "revision": "8c5a6768daf5f0c76d1a25111a0535c9",
+    "url": "/Portfolio/static/media/icons8-nodejs.8c5a6768.svg"
+  },
+  {
+    "revision": "88934bb35ebe8dda6e2a",
+    "url": "/Portfolio/static/js/main.88934bb3.chunk.js"
+  },
+  {
+    "revision": "6c3cbb01d5d2afa13e14c536d7af06e2",
+    "url": "/Portfolio/static/media/MTG.6c3cbb01.png"
+  },
+  {
+    "revision": "c78389c268ae63c10d321f6e0141fe7c",
+    "url": "/Portfolio/static/media/TaskRabbit.c78389c2.png"
+  },
+  {
+    "revision": "8cd498fff05838883720691a24bd2af5",
+    "url": "/Portfolio/static/media/TrMessages.8cd498ff.png"
+  },
+  {
+    "revision": "37bb7ae1ce64115b5bb76600bc47dbf3",
+    "url": "/Portfolio/static/media/TRForm.37bb7ae1.png"
+  },
+  {
+    "revision": "d745c5f8a4d8ab65118804f4b2a15242",
+    "url": "/Portfolio/static/media/magicBuilder.d745c5f8.png"
+  },
+  {
+    "revision": "6307cbbbc02237a36224911a8507418d",
+    "url": "/Portfolio/static/media/magicDecks.6307cbbb.png"
+  },
+  {
+    "revision": "982918f38c8c5f4679f725650dde0e97",
+    "url": "/Portfolio/static/media/profile_image.982918f3.jpg"
+  },
+  {
+    "revision": "71c3d6c057e437fb339ff195954c3ba4",
+    "url": "/Portfolio/static/media/TRAccount.71c3d6c0.png"
+  },
+  {
+    "revision": "4079dba3424feb6f951a6ab023199288",
+    "url": "/Portfolio/index.html"
+  }
+];
